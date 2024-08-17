794a52210930b91a784092d0f0a3531a69c132eb
Shourya Bose <sbose@gpu6.lcrc.anl.gov>
2024-08-17 17:35:54 -0500

    update

--- files_for_appfl/loss.py
+++ files_for_appfl/loss.py
@@ -2,12 +2,21 @@ import torch.nn as nn
 
 class MSELoss(nn.Module):
     
-    def __init__(self):
+    def __init__(self, normalization_type='minmax'):
         super(MSELoss,self).__init__()
         self.loss = nn.MSELoss()
+        self.normalization_type = normalization_type
         
     def forward(self, target, prediction):
-        target0 = target[:,:,0].unsqueeze(-1)
-        min,max = target[:,:,1].unsqueeze(-1), target[:,:,2].unsqueeze(-1)
-        target0 = (target0 - min) / (max-min)
+        if self.normalization_type == 'minmax':
+            target0 = target[:,:,0].unsqueeze(-1)
+            min,max = target[:,:,1].unsqueeze(-1), target[:,:,2].unsqueeze(-1)
+            target0 = (target0 - min) / (max-min)
+        else:
+            if self.normalization_type == 'z':
+                target0 = target[:,:,0].unsqueeze(-1)
+                mean, std = target[:,:,1].unsqueeze(-1), target[:,:,2].unsqueeze(-1)
+                target0 = (target0 - mean) / std
+            else:
+                raise ValueError('normalize_type must be either of <minmax> or <z>')
         return self.loss(prediction,target0)
--- files_for_appfl/metric.py
+++ files_for_appfl/metric.py
@@ -2,11 +2,19 @@ import numpy as np
 
 EPSILON = 1e-5
 
-def mape(target, prediction):
+def mape(target, prediction, normalization_type = 'minmax'):
     
-    target0 = np.expand_dims(target[:,:,0], axis=-1)
-    min, max = np.expand_dims(target[:,:,1], axis=-1), np.expand_dims(target[:,:,2], axis=-1)
-    prediction = (max-min)*prediction + min
+    if normalization_type == 'minmax':
+        target0 = np.expand_dims(target[:,:,0], axis=-1)
+        min, max = np.expand_dims(target[:,:,1], axis=-1), np.expand_dims(target[:,:,2], axis=-1)
+        prediction = (max-min)*prediction + min
+    else:
+        if normalization_type == 'z':
+            target0 = np.expand_dims(target[:,:,0], axis=-1)
+            mean, std = np.expand_dims(target[:,:,1], axis=-1), np.expand_dims(target[:,:,2], axis=-1)
+            prediction = std*prediction + mean
+        else:
+            raise ValueError('normalize_type must be either of <minmax> or <z>')
     
     num = np.abs(target0 - prediction)
     den = np.abs(target0) + EPSILON
